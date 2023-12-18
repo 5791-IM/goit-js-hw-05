22b8cdfb35d6f2d2093b3c331478eba4967aa301
5791-IM <142788119+5791-IM@users.noreply.github.com>
2023-12-18 20:58:10 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,8 +1,7 @@
-const getTotalBalanceByGender = (users, gender) => {
-  return users
+const getTotalBalanceByGender = (users, gender) =>
+  users
     .filter((user) => user.gender === gender)
     .reduce((totalBalance, user) => totalBalance + user.balance, 0);
-};
 
 const allUsers = [
   {
